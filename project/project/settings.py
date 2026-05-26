4ad53bebf7bfb028b64f2438e2d9b3dbce91bc53
Paul-Bearer <jhdohertyiii@gmail.com>
2026-05-26 17:53:57 -0300

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -142,4 +142,25 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
 
